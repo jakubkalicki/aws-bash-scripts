fe882999e6e62e0b0e2067366b711da5d7b29b4f
Jakub Kalicki <jakub.kalicki@pm.me>
2019-06-24 01:45:17 +0200

    Save filtering output as array + Cleanup

--- update_aws_sg.sh
+++ update_aws_sg.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # Usage: ./update_aws_sg.sh <security group id> <protocol> <port> <rule description>
 
@@ -26,42 +26,47 @@ my_ip="$(curl -s curlmyip.org || echo "Failed")";
 [ "${my_ip}" == "Failed" ] \
  && exit_with_error "Failed to retrieve my IP from v4.ifconfig.co";
 
-# Use "" as external tags to let multiple string in rule_description
-# Use '' for strings (it's a short for `""`)
-# Use \` for numbers (has to be escaped because it's inside of "")
-aws ec2 describe-security-groups \
+my_cidr="${my_ip}/32";
+
+
+# Fetch inbound rules for specified security group id filtering by rule's port, protocol and description
+
+# Using "" as external tags to let multiple string in rule_description
+# Using '' for strings (it's a short for `""`)
+# Using \` for numbers (has to be escaped because it's inside of "")
+# Removing \r (carriage return) with sed cmd
+mapfile -t allowed_cidrs < <(aws ec2 describe-security-groups \
                    --output text \
                    --query "SecurityGroups[?GroupId=='${group_id}']\
  | [0].IpPermissions[?ToPort==\`${port}\` && FromPort==\`${port}\` && IpProtocol==\`${protocol}\`]\
  | [0].IpRanges[?Description!='null' && Description=='${rule_description}'].CidrIp"\
- || echo "Failed" > allowed_cidrs;
+ | sed 's/\r$//' \
+ || echo "Failed");
+
 
 [ "${allowed_cidrs}" == "Failed" ] \
- && exit_with_error "Failed to retrieve SSH ingress rules for ${group_id}";
-
-# With my_ip and allowed_cidrs known, clean-house by revoking all access that isn't from here.
-my_cidr="${my_ip}/32";
+ && exit_with_error "Failed to fetch inbound rules for ${group_id}";
 
 for cidr in ${allowed_cidrs}; do
 # Don't quote this string, bash needs to tokenise it and it's not an array.
     if [ "${cidr}" == "${my_cidr}" ]; then
         access_granted="true";
     else
-        echo -en "Revoking SSH access to ${group_id} from ${cidr}... ";
+        echo -en "Revoking inbound rule in group ${group_id} for ${cidr}... ";
         aws ec2 revoke-security-group-ingress \
  --group-id ${group_id} \
  --protocol ${protocol} \
  --port ${port} \
  --cidr ${cidr} \
- && echo -e "Done." \
- || echo -e "Failed.";
+ && echo -e "Done" \
+ || echo -e "Failed";
     fi;
 done;
 
 if [ "${access_granted}" == "true" ]; then
-    echo -e "Access already authorised from ${my_cidr}";
+    echo -e "Inbound rule already exists for ${my_cidr}";
 else
-    echo -en "Adding inbound rule in group ${group_id} for ${my_cidr}...\n";
+    echo -en "Adding inbound rule in group ${group_id} for ${my_cidr}... ";
     aws ec2 authorize-security-group-ingress \
  --group-id ${group_id} \
  --ip-permissions IpProtocol="${protocol}",FromPort="${port}",ToPort="${port}",\
